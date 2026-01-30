ff09f3662f882c4f47e2f0aeaef3663d651a7297
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-01-29 22:52:37 -0300

    style: add ruff to lint and formatting

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import functions_framework
+from flask import Request
 
 
 @functions_framework.http
-def starwars_func(request):
-    if request.method == "OPTIONS":
+def starwars_func(request: Request) -> tuple:
+    if request.method == 'OPTIONS':
         headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Max-Age": "3600",
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600',
         }
-        return ("", 204, headers)
+        return ('', 204, headers)
 
-    headers = {"Access-Control-Allow-Origin": "*"}
+    headers = {'Access-Control-Allow-Origin': '*'}
 
-    return ("Hello World!", 200, headers)
+    return ('Hello World!', 200, headers)
