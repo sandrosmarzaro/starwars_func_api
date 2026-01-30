d7ccdbd7c553b174e79b95c37babb3e43fcca58e
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-01-30 15:55:34 -0300

    refactor: use request validator in func

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from http import HTTPStatus
+
 import functions_framework
-from flask import Request
+from flask import Request, Response, jsonify
+from loguru import logger
+
+from validators.request_validator import RequestValidator
 
 
 @functions_framework.http
-def starwars_func(request: Request) -> tuple:
+def starwars_func(request: Request) -> tuple[Response, int, dict]:
     if request.method == 'OPTIONS':
         headers = {
             'Access-Control-Allow-Origin': '*',
@@ -11,8 +16,14 @@ def starwars_func(request: Request) -> tuple:
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Max-Age': '3600',
         }
-        return ('', 204, headers)
+        return (jsonify({}), HTTPStatus.NO_CONTENT.value, headers)
 
     headers = {'Access-Control-Allow-Origin': '*'}
 
-    return ('Hello World!', 200, headers)
+    logger.debug(request.path)
+
+    is_valid_request, error = RequestValidator.validate(request)
+    if not is_valid_request and error is not None:
+        return (error.to_response(), error.status, headers)
+
+    return (jsonify({'message': 'Hello World!'}), HTTPStatus.OK.value, headers)
