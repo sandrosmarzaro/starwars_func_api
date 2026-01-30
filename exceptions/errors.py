1839b03fa0469eb2e1baca2690fb567cd8bc8352
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-01-30 15:52:43 -0300

    chore: improve the ignore files

---
+++ exceptions/errors.py
@@ -0,0 +1,30 @@
+from http import HTTPStatus
+
+from flask import Response, jsonify
+
+
+class BaseError:
+    name: str
+    message: str
+    status: int
+
+    @classmethod
+    def to_response(cls) -> Response:
+        return jsonify(
+            {
+                'error': cls.name,
+                'message': cls.message,
+            }
+        )
+
+
+class MethodNotAllowedError(BaseError):
+    name = HTTPStatus.METHOD_NOT_ALLOWED.name
+    message = HTTPStatus.METHOD_NOT_ALLOWED.description
+    status = HTTPStatus.METHOD_NOT_ALLOWED.value
+
+
+class NotFoundError(BaseError):
+    name = HTTPStatus.NOT_FOUND.name
+    message = HTTPStatus.NOT_FOUND.description
+    status = HTTPStatus.NOT_FOUND.value
