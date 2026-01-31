e72f970cb9877cdbb3b2d4174ebfeb4e713db5b0
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-01-30 23:15:47 -0300

    feat: use pydantic settings to management the environments variables

---
+++ infra/settings.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class _Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore',
+    )
+    SWAPI_BASE_URL: str = 'https://swapi.dev/api'
+
+
+@lru_cache
+def _get_settings() -> _Settings:
+    return _Settings()
+
+
+settings = _get_settings()
