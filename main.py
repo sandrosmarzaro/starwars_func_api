f1433e50241f0e22b049102be48367f352ad911a
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-01-30 23:16:57 -0300

    feat: create get func to retrieve data from swapi

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from http import HTTPStatus
+from urllib.parse import urljoin
 
 import functions_framework
+import httpx
 from flask import Request, Response, jsonify
 from loguru import logger
 
+from infra.settings import settings
 from validators.request_validator import RequestValidator
 
 
 @functions_framework.http
 def starwars_func(request: Request) -> tuple[Response, int, dict]:
+    logger.debug(f'{request.path}, {request.args}')
+
     if request.method == 'OPTIONS':
         headers = {
             'Access-Control-Allow-Origin': '*',
@@ -20,10 +25,30 @@ def starwars_func(request: Request) -> tuple[Response, int, dict]:
 
     headers = {'Access-Control-Allow-Origin': '*'}
 
-    logger.debug(request.path)
-
     is_valid_request, error = RequestValidator.validate(request)
     if not is_valid_request and error is not None:
         return (error.to_response(), error.status, headers)
 
-    return (jsonify({'message': 'Hello World!'}), HTTPStatus.OK.value, headers)
+    swapi_data = _get_swapi_data(request.args)
+
+    return (swapi_data, HTTPStatus.OK.value, headers)
+
+
+def _get_swapi_data(params: dict) -> Response:
+    resource = params.get('resource')
+    resource_id = params.get('id')
+    base_url = urljoin(settings.SWAPI_BASE_URL, resource)
+
+    if resource_id:
+        base_url = urljoin(base_url, resource_id)
+
+    query_params = {
+        key: params[key] for key in ('search', 'page') if params.get(key)
+    }
+
+    with httpx.Client() as client:
+        response = client.get(base_url, params=query_params)
+        response.raise_for_status()
+
+    logger.debug(f'{response}, {response.json()}')
+    return jsonify(response.json())
