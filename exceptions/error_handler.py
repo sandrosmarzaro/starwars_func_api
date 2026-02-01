74c2355e24f317c40b413e2d9d286cc8a3aac669
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-02-01 17:46:03 -0300

    feat: create error handler to pydantic

--- exceptions/error_handler.py
+++ exceptions/error_handler.py
@@ -4,6 +4,7 @@ import functions_framework
 from flask import Flask, Response, jsonify
 from httpx import HTTPError, HTTPStatusError
 from loguru import logger
+from pydantic import ValidationError
 
 from exceptions.errors import (
     BadRequestError,
@@ -41,13 +42,44 @@ def _handle_httpx_error(exc: HTTPError) -> tuple[Response, int, dict]:
     return (jsonify(error.to_dict()), error.status, headers)
 
 
+def _handle_api_error(exc: BaseError) -> tuple[Response, int, dict]:
+    logger.warning(exc)
+    headers = build_cors_headers()
+
+    return (jsonify(exc.to_dict()), exc.status, headers)
+
+
+def _handle_validation_error(
+    exc: ValidationError,
+) -> tuple[Response, int, dict]:
+    logger.warning(exc)
+    error = BadRequestError()
+    headers = build_cors_headers()
+
+    return (jsonify(error.to_dict()), error.status, headers)
+
+
 def register_error_handlers(app: Flask) -> None:
     app.register_error_handler(HTTPError, _handle_httpx_error)
+    app.register_error_handler(BaseError, _handle_api_error)
+    app.register_error_handler(ValidationError, _handle_validation_error)
 
 
 @functions_framework.errorhandler(HTTPError)
 def handle_httpx_errors(exc: HTTPError) -> tuple[Response, int, dict]:
     return _handle_httpx_error(exc)
+
+
+@functions_framework.errorhandler(BaseError)
+def handle_api_erros(exc: BaseError) -> tuple[Response, int, dict]:
+    return _handle_api_error(exc)
+
+
+@functions_framework.errorhandler(ValidationError)
+def handle_validation_error(
+    exc: ValidationError,
+) -> tuple[Response, int, dict]:
+    return _handle_validation_error(exc)
 
 
 def setup() -> None:
