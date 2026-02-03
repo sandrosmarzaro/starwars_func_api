ce89c2adc7f22cbbe0f203a3430f8deb60e7eb7f
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-02-02 22:27:36 -0300

    feat: user expand service in swapi service

--- services/swapi_data_service.py
+++ services/swapi_data_service.py
@@ -6,11 +6,13 @@ from loguru import logger
 
 from infra.settings import settings
 from schemas.swapi_query_params_schema import SwapiQueryParams
+from services.expand_swapi_data_service import ExpandSwapiDataService
 
 
 class SwapiDataService:
     def __init__(self) -> None:
         self.base_url = settings.SWAPI_BASE_URL
+        self.expand_service = ExpandSwapiDataService()
 
     async def get_swapi_data(self, params: SwapiQueryParams) -> dict[str, Any]:
         resource_url = urljoin(self.base_url, f'{params.resource}/')
@@ -28,6 +30,10 @@ class SwapiDataService:
         async with httpx.AsyncClient() as client:
             response = await client.get(resource_url, params=query_params)
             response.raise_for_status()
+            data = response.json()
 
-        logger.debug(f'{response}, {response.json()}')
-        return response.json()
+            if params.expand:
+                data = await self.expand_service.expand(client, data)
+
+        logger.debug(f'{response}, {data}')
+        return data
