39832f015f4c48905447d65a14261365c1f449c0
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-02-04 15:09:27 -0300

    refactor: use aside cache in swapi service

--- services/swapi_data_service.py
+++ services/swapi_data_service.py
@@ -1,22 +1,53 @@
-from typing import Any
+from typing import Annotated
 from urllib.parse import urljoin
 
 import httpx
+from fastapi import Depends
 from loguru import logger
 
 from infra.settings import settings
+from repositories.cache_repository import CacheRepository
 from schemas.swapi_query_params_schema import SwapiQueryParams
 from services.expand_swapi_data_service import ExpandSwapiDataService
 from services.sort_swapi_data_service import SortSwapiDataService
 
 
 class SwapiDataService:
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        cache_repository: Annotated[CacheRepository, Depends()],
+    ) -> None:
         self.base_url = settings.SWAPI_BASE_URL
         self.expand_service = ExpandSwapiDataService()
         self.sort_service = SortSwapiDataService()
+        self.cache_repository = cache_repository
 
-    async def get_swapi_data(self, params: SwapiQueryParams) -> dict[str, Any]:
+    async def get_swapi_data(
+        self, params: SwapiQueryParams
+    ) -> dict[str, object]:
+        cached_data = self.cache_repository.get(params)
+        if cached_data is not None:
+            data = cached_data
+        else:
+            data = await self._fetch_from_swapi(params)
+            self.cache_repository.set(params, data)
+
+        if params.expand:
+            async with httpx.AsyncClient() as client:
+                data = await self.expand_service.expand(
+                    client, data, params.expand
+                )
+
+        if params.sort_by:
+            data = self.sort_service.sort(
+                data, params.sort_by, params.sort_order
+            )
+
+        return data
+
+    async def _fetch_from_swapi(
+        self, params: SwapiQueryParams
+    ) -> dict[str, object]:
         resource_url = urljoin(self.base_url, f'{params.resource.value}/')
 
         if params.id:
@@ -33,16 +64,6 @@ class SwapiDataService:
             response = await client.get(resource_url, params=query_params)
             response.raise_for_status()
             data = response.json()
-
-            if params.expand:
-                data = await self.expand_service.expand(
-                    client, data, params.expand
-                )
-
-        if params.sort_by:
-            data = self.sort_service.sort(
-                data, params.sort_by, params.sort_order
-            )
 
         logger.debug(f'{response}, {data}')
         return data
